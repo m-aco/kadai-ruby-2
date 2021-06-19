a30c77c6416b2674bea6be5c263bd87550262cc2
m-aco <m.aco02105@gmail.com>
2021-06-19 14:07:25 +0000

    kadai-fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,16 @@
+num_max = 100
+(1..num_max).each do |num|
+
+def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+        "FizzBuzz"
+    elsif num % 5 == 0
+        "Buzz"
+    elsif num % 3 == 0
+        "Fizz"
+    else
+        return num
+    end
+end
+puts fizzbuzz(num)
+end
